0b4b0a84ecb9ed08a8f799ce24f2c549ec2c019b
Eitan Charytan-Spitz <eitands@gmail.com>
2020-09-22 17:58:19 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,6 +1,6 @@
 def find_element_index(array, value_to_find)
   array.size.times do |counter|
-    if array[counter] = value_to_find
+    if array[counter] == value_to_find
       return counter
     end
   end
